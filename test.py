111deb2acd7e08ac698e3280e11e0641590b087e
mikn24 <m.nietlispach@hotmail.com>
2025-08-01 12:27:46 +0200

    init

---
+++ test.py
@@ -0,0 +1,21 @@
+
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+# This function runs for the homepage URL ('/')
+@app.route('/')
+def index():
+    return '<h1>Welcome to the main page!</h1>'
+
+# This function runs for the '/about' URL
+@app.route('/about')
+def about_page():
+    return '<h2>This is the about page.</h2>'
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+    
