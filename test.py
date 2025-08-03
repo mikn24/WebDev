454582b6da04049a4c3311ba405f0b180f3aee05
mikn24 <m.nietlispach@hotmail.com>
2025-08-03 15:01:39 +0200

    1501

--- test.py
+++ test.py
@@ -15,8 +15,16 @@ def home():
     return render_template('welcome_test.html')
 
 
+
+@app.route('/about')
+def level1():
+    return render_template('level1_test.html')
+
+
+
 if __name__ == '__main__':
     app.run(debug=True,host="0.0.0.0", port=8000)
+
 
 
 
